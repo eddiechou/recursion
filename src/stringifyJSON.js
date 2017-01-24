7f3136772f0313b00e3e2d7fab2c6e581b8fedca
Eddie Chou <eddieechou@gmail.com>
2017-01-23 18:15:27 -0800

    Set up switch statement for all data types for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  switch(typeof obj) {
+  	case 'boolean':
+  		return (obj === true) ? "true" : "false";
+  	case 'number':
+  		return '' + obj;
+  		break;
+  	case 'object':
+  		if (obj === null) {
+  			return 'null';
+  		}
+  		break;
+  	case 'string':
+  		return '"' + obj + '"';
+  		break;
+  	case 'undefined':
+  		return 'undefined';
+  		break;
+  	case 'function':
+  		return '';
+  		break;
+
+
+  }
 };
