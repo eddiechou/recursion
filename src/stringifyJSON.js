3ef876b97c21e82f9e90c7511f829a7b3835a5b5
Eddie Chou <eddieechou@gmail.com>
2017-01-23 19:00:19 -0800

    Implement stringifyJSON for arrays and objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,29 +4,70 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // If array
+	if (Array.isArray(obj)) {
+  	var result = '[';
+  	if (obj.length) {
+  		result += stringifyJSON(obj[0]);
+  	}
+  	for (let i=1; i<obj.length; i++) {
+  		result += ',' + stringifyJSON(obj[i]);
+  	}
+  	result += ']';
+  	return result;
+  } else {
+  	switch(typeof obj) {
 
-  switch(typeof obj) {
-  	case 'boolean':
-  		return (obj === true) ? "true" : "false";
-  	case 'number':
-  		return '' + obj;
-  		break;
-  	case 'object':
-  		if (obj === null) {
-  			return 'null';
-  		}
-  		break;
-  	case 'string':
-  		return '"' + obj + '"';
-  		break;
-  	case 'undefined':
-  		return 'undefined';
-  		break;
-  	case 'function':
-  		return '';
-  		break;
+  		case 'boolean':
+  			return (obj === true) ? "true" : "false";
 
+  		case 'number':
+  			return '' + obj;
+  			break;
 
+  		case 'function':
+  			return 'function';
+  			break;
+
+  		case 'object':
+  			if (obj === null) {
+  				return 'null';
+  			} else if (Object.keys(obj).length === 0) {
+  				return "{}";
+  			} else {
+				var result = '{';
+				var firstProperty = true;
+  				for (var prop in obj) {
+  					// Extra check in case property is in prototype
+  					if(obj.hasOwnProperty(prop)){
+  						if(prop === "functions" || prop === "undefined") continue;
+  						if(!firstProperty){
+  							result += ','
+  						}
+  						// Concatenate property key
+  						result += '"' + prop + '":';
+  						// Concatenate property value
+  						if(typeof obj[prop] === 'string') {
+  							result += '"' + obj[prop] + '"';
+  						} else {
+  							result += stringifyJSON(obj[prop]);
+  						}
+  						firstProperty = false;
+  					}
+
+  				}
+  				result += '}';
+  				return result;
+  			}
+  			break;
+
+  		case 'string':
+  			return '"' + obj + '"';
+  			break;
+
+  		case 'undefined':
+  			return 'undefined';
+  			break;
+  	}
   }
 };
